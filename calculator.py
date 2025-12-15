ada1e79ae191c9ae749db726ae9615b0c2eda24e
Basilio0o <basiliooo007@gmail.com>
2025-12-15 12:45:02 +0300

    Добавлены функции умножения и вычитания

--- calculator.py
+++ calculator.py
@@ -4,7 +4,14 @@ def divide(a, b):
 	if b == 0:
  		raise ValueError("Деление на ноль невозможно")
 	return a / b
+def multiply(a, b):
+	return a * b
+def subtract(a, b):
+	return a - b
+ 
 a = int(intput())
 b = int(intput())
 print(add(a,b))
 print(divide(a,b))
+print(multiply(a,b))
+print(subtract(a,b))
