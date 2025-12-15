895378be111ccda39a2fe30c5ef3107494df7667
Basilio0o <basiliooo007@gmail.com>
2025-12-15 12:36:10 +0300

    Добавлена функция деления

--- calculator.py
+++ calculator.py
@@ -1,5 +1,10 @@
 def add(a, b):
 	return a + b
+def divide(a, b):
+	if b == 0:
+ 		raise ValueError("Деление на ноль невозможно")
+	return a / b
 a = int(intput())
 b = int(intput())
 print(add(a,b))
+print(divide(a,b))
